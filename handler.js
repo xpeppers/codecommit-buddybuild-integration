abf6a77303752be347fba570986fc57878723662
Alessio Coser <alessio.coser@xpeppers.com>
2017-09-04 17:23:56 +0200

    first lambda test

---
+++ handler.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const request = require('request')
+
+module.exports.launchBuild = (event, context, callback) => {
+  console.log(JSON.stringify(event))
+  const APP_ID = 'APP_ID'
+  const ACCESS_TOKEN = 'ACCESS_TOKEN'
+
+  request({
+    headers: {'Authorization': `Bearer ${ACCESS_TOKEN}`},
+    uri: `https://api.buddybuild.com/v1/apps/${APP_ID}/build`,
+    method: 'POST'
+  }, function (err, res, body) {
+    if (err) {
+      console.log('response_error: ', err)
+    }
+    console.log('response: ', JSON.stringify(res))
+    console.log('response_body: ', body)
+    context.succeed()
+  })
+}
