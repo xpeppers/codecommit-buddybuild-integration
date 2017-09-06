c5bf209cdb2340f90cfbfc6fc56adbe368547163
Alessio Coser <alessio.coser@xpeppers.com>
2017-09-06 17:01:00 +0200

    extract buddybuild class

--- handler.js
+++ handler.js
@@ -1,35 +1,13 @@
 'use strict'
 
-const request = require('request')
-const CodeCommitEvent = require('../lib/codecommit-event')
+const CodeCommitEvent = require('./lib/codecommit-event')
+const BuddyBuild = require('./lib/buddybuild')
 
 module.exports.launchBuild = (event, context) => {
-  var branches = new CodeCommitEvent(event).getBranches()
+  var branches = new CodeCommitEvent(event).branches()
+  var buddyBuild = new BuddyBuild()
 
-  return Promise.all(branches.map(launchBuddyBuild))
-  .then(() => {
-    context.succeed()
-  })
-  .catch(() => {
-    context.fail()
-  })
-}
-
-function launchBuddyBuild (branch) {
-  return new Promise((resolve, reject) => {
-    request({
-      headers: {'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`},
-      uri: `https://api.buddybuild.com/v1/apps/${process.env.APP_ID}/build`,
-      data: {branch: branch},
-      method: 'POST'
-    }, function (err, res, body) {
-      if (err) {
-        console.log('response_error: ', err)
-        resolve(err)
-      } else {
-        console.log(body)
-        resolve(body)
-      }
-    })
-  })
+  return Promise.all(branches.map((branch) => buddyBuild.triggerFor(branch)))
+  .then(context.succeed)
+  .catch(context.fail)
 }
